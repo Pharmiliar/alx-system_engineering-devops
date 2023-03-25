bb38cd764229bf739b507778d4853a641ada696a
Pharmiliar <iam.onyidia@gmail.com>
2023-03-25 04:52:01 +0100

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Installs puppet-flask, version 2.1.0
+# Installs puppet-lint, version 2.1.1
 
-package { 'puppet-flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
